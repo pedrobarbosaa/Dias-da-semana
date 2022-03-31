6e92bb44aa1164b5b5c5d12c8f4fabf982e6587b
pedrobarbosaa <102681346+pedrobarbosaa@users.noreply.github.com>
2022-03-31 00:33:03 -0300

    dias da semana

---
+++ Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main (String[] args){
+    int dia = 2;
+    switch (dia) {
+        case 1:
+        System.out.println ("segunda");
+break;
+
+case 2:
+System.out.println("terça");
+break;
+
+case 3:
+System.out.println("quarta");
+break;
+
+case 4:
+System.out.println("quinta");
+break;
+
+case 5:
+System.out.println("sexta");
+break;
+
+case 6:
+System.out.println("sábado");
+break;
+
+case 7:
+System.out.println("domingo"); }
+}
+
+}
